1bef260cf35b987a9f1519335dbc8dc6022c5367
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-27 15:56:33 -0700

    re uploaded package.json

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Noted
+
+## Description
+The motivation behind this project was to create a website for small business owners to save notes and keep themselves organized. The front end of this project had already been given and I was tasked with connecting the front end and back end together.
+
+
+# Technology Used 
+JavaScript| HTML| CSS
+## Installation
+In order to install this feature you should do the following:
+
+Download Node onto your computer Fork the repo from github Clone the repo from github Run "npm install" Run "npm install "
+
+## Test
+N/A
+
+## License
+N/A
--- server.js
+++ server.js
@@ -1,43 +1,59 @@
+//import required modules
 const express = require('express');
 const app = express();
 
 const fs = require('fs');
 const path = require('path');
-const PORT = 3001;
 
-const allNotes = require('./db/db.json');
+//port number to listen on
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+//import array of notes from JSON file
+let allNotes = require('./db/db.json');
+
+//configure express middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//get all notes
 app.get('/api/notes', (req, res) => {
     res.json(allNotes.slice(1));
 });
 
+//serve home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//serve note page
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+//search-all route for non-existent paths
 app.get('*', (req, res) => {
-    res.sendFile(path.joinf(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//add a new note
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    if(!Array.isArray(allNotes))
-    allNotes = [];
 
-    if(allNotes.length === 0)
-    allNotes.push(0);
+     //if array of notes hasn't been initialized, create an empty array
+    if(!Array.isArray(allNotes)) {
+        allNotes = [];
+    }
 
-    newNote.id = allNotes[0];
-    allNotes[0]++
+    //if no new notes add counter to beginning of array
+    if(allNotes.length === 0) {
+        allNotes.push({ id: 1, title: "", text: "" }); // Changed counter to an object with a default note
+    }
 
+    //assign id to new note and increment the counter
+    newNote.id = allNotes[allNotes.length - 1].id + 1;
+
+    //add new note to array and write to JSON file
     allNotes.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
@@ -46,15 +62,18 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
 });
 
+//delete note by id
 app.delete('/api/notes/:id', (req, res) => {
     const id = req.params.id;
+
+    //loop through all notes to find matching ID
     for(let i = 0; i < allNotes.length; i++) {
         let note = allNotes[i];
 
         if (note.id == id) {
             allNotes.splice(i, 1);
             fs.writeFileSync(
-                path.join(__dirname, 'db/db/json'),
+                path.join(__dirname, './db/db.json'),
                 JSON.stringify(allNotes, null, 2)
             );
             break;
@@ -63,6 +82,7 @@ app.delete('/api/notes/:id', (req, res) => {
     res.json(true);
 });
 
+//start server 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
